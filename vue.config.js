94f24bef3e618a8b406e7ee93affa9ac5e029a9e
Alessandro <alessandro.macri95@gmail.com>
2023-02-26 16:43:30 +0100

    update gh-pages-deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/alternativa-takeaway-app/',
+  publicPath: '/alternativa-takeaway-app/',
   transpileDependencies: true
 })
