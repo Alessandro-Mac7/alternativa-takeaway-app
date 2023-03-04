d176c5c189853ed5eacb8d58666ba021f3e90949
Alessandro <alessandro.macri95@gmail.com>
2023-03-04 18:11:56 +0100

    update cicd

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
+function getPublicPath() {
+  switch (process.env.NODE_ENV) {
+    case 'development': return '/alternativa-takeaway-app/'
+    default: return '/'
+  }
+}
 module.exports = defineConfig({
-  publicPath: '/alternativa-takeaway-app/',
+  publicPath: getPublicPath(),
   transpileDependencies: true
 })
